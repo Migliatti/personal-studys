f9de4e6c30f57430e0a278168d9110892f7b7a47
Gabriel Migliatti <gabimpariz@icloud.com>
2026-09-03 20:36:59 -0300

    feat: add initial JavaScript practice program

---
+++ 00-logica-javascript/exercicios/pratica/missao-1.mjs
@@ -0,0 +1,5 @@
+const pessoa = "Gustavo"
+const aprendizado = "Biologia"
+
+console.log("Acaba de chegar:", pessoa)
+console.log("O(a)",pessoa, "quer aprender:", aprendizado)
--- 00-logica-javascript/exercicios/pratica/primeiro-programa.mjs
+++
@@ -1,1 +0,0 @@
-console.log("Comecei minha prática em JavaScript")
